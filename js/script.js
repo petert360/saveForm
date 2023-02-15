8c907655dc4f738f2c86f6512add2d6a03b720aa
petert360 <peter.t360@hotmail.com>
2023-02-15 03:18:23 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycbwDMcqOOuD_oJwpAhenPmtTzmLQOx820z2eGwC5nVNsCaqloqFYgH7oPwrBvPEikixW/exec';
+const form = document.forms['anesthesiaForm'];
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message));
+});
