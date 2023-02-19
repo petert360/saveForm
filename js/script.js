a871fcb0a68f4a052dbc48915ec181d2d1c113f2
petert360 <peter.t360@hotmail.com>
2023-02-19 19:44:03 +0100

    update: style, new features

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ const form = document.forms['anesthesiaForm'];
 form.addEventListener('submit', e => {
   e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message));
+    .then(response => {
+      console.log('Success!', response);
+      window.alert('Sikeres küldés!');
+    })
+    .catch(error => {
+       console.error('Error!', error.message)
+       window.alert('Hibás küldés!');
+      });
 });
