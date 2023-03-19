38457c04ad4da53d3224ceae7dd027075f98411a
petert360 <peter.t360@hotmail.com>
2023-03-19 14:03:01 +0100

    add modal

--- js/script-3.js
+++ js/script-3.js
@@ -9,11 +9,13 @@ form.addEventListener('submit', e => {
       //window.localStorage.removeItem('form');
       //Küldésnél törlés - kikapcsolva: form.reset();
       console.log('Success!', response);
-      window.alert('Sikeres küldés!');
+      //window.alert('Sikeres küldés!');
+      showModal('Sikeres küldés');
     })
     .catch(error => {
       console.error('Error!', error.message);
-      window.alert('Hibás küldés!');
+      //window.alert('Hibás küldés!');
+      showModal('Hibás küldés');
     });
 });
 
@@ -183,4 +185,39 @@ function setAnthropometry(bmi, ibw) {
 function setFormIdElement(id) {
   formIdNumber = id;
   formIdElement.innerText = id;
+}
+
+// THE MODAL
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// Get the modal-message div
+const modalMessage = document.querySelector('.modal-message');
+
+// When the user clicks the button, open the modal
+/*btn.onclick = function () {
+  modal.style.display = 'block';
+};*/
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
+
+function showModal(msg) {
+  modalMessage.innerText = msg;
+  modal.style.display = 'block';
 }
